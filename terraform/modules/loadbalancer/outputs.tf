b2786ee9c9b5def816b03e8dafaca7044e73f7c9
vdonkor <vincent.donkor@gmail.com>
2022-06-04 14:20:31 -0400

    Develop (#4)
    fixed unused vars

--- terraform/modules/loadbalancer/outputs.tf
+++ terraform/modules/loadbalancer/outputs.tf
@@ -2,9 +2,6 @@ output "alb_dns_name" {
   value = aws_lb.alb.dns_name
   description = "ALB dns name"
 }
-output "alb_security_group_id" {
-  value = aws_security_group.alb-sg.id
-}
 output "alb_https_listener_arn" {
   description = "https listener arn"
   value = aws_lb_listener.listener_https.arn
