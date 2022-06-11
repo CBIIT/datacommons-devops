7424d604a34b2566b89d1c8fec2ff0a9151ceb94
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-06-11 15:33:39 -0400

    changed default vars

--- terraform/modules/roles/main.tf
+++ terraform/modules/roles/main.tf
@@ -4,22 +4,23 @@ locals {
 
 module "iam_assumable_role" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
-  version = "~> 4"
   trusted_role_arns = var.trusted_role_arns
-  create_role = true
+  create_role = var.create_role
   role_name  = var.iam_role_name
   role_requires_mfa = false
   custom_role_policy_arns = local.all_custom_policy_arns
   number_of_custom_role_policy_arns = length(local.all_custom_policy_arns)
   trusted_role_services = var.trusted_role_services
+  role_description = var.role_description
+  tags = var.tags
 }
 
 module "iam_policy" {
-  count = var.add_custom_policy ? 1 : 0
+  count   = var.add_custom_policy ? 1 : 0
   source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
-  version = "~> 4"
-  name        =  var.custom_policy_name
-  path        = "/"
+  version = "5.1.0"
+  name    =  var.custom_policy_name
   description = var.iam_policy_description
   policy = var.iam_policy
+  tags = var.tags
 }
--- terraform/modules/roles/variables.tf
+++ terraform/modules/roles/variables.tf
@@ -1,31 +1,35 @@
 variable "custom_role_policy_arns" {
   description = "list of policy arns defined for this role"
   type = list(string)
-  default = null
+  default = []
 }
 variable "trusted_role_arns" {
   description = "list of trusted roles arns"
   type = list(string)
-  default = null
+  default = []
 }
 variable "iam_role_name" {
   description = "name of the role"
   type = string
 }
 variable "add_custom_policy" {
-  description = "add other policies other than managed policy"
+  description = "add policies other than managed policy"
   type = bool
   default = false
 }
 variable "custom_policy_name" {
   description = "name of custom policy"
   type = string
-  default = null
 }
 variable "iam_policy_description" {
   description = "description of iam policy"
   type = string
   default = null
+}
+variable "create_role" {
+  description = "create iam role or not"
+  type = bool
+  default = true
 }
 variable "iam_policy" {
   description = "iam policy document"
@@ -36,4 +40,14 @@ variable "trusted_role_services" {
   default =  null
   description = "list of trusted service"
   type = list(string)
+}
+variable "tags" {
+  description = "resource tags"
+  type = map(string)
+  default = null
+}
+variable "role_description" {
+  description = "description of the role"
+  type = string
+  default = "iam role"
 }
