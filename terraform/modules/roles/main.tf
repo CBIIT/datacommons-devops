89b3ebc2d56e343f03aa09c75dc6a7217cf775f3
vdonkor <vincent.donkor@gmail.com>
2022-06-11 13:01:01 -0400

    Develop (#7)
    lgtm

---
+++ terraform/modules/roles/main.tf
@@ -0,0 +1,24 @@
+locals {
+  all_custom_policy_arns = var.add_custom_policy ? concat([ module.iam_policy[0].arn],var.custom_role_policy_arns) : var.custom_role_policy_arns
+}
+
+module "iam_assumable_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+  version = "~> 4"
+  trusted_role_arns = var.trusted_role_arns
+  create_role = true
+  role_name  = var.iam_role_name
+  role_requires_mfa = false
+  custom_role_policy_arns = local.all_custom_policy_arns
+  number_of_custom_role_policy_arns = length(local.all_custom_policy_arns)
+}
+
+module "iam_policy" {
+  count = var.add_custom_policy ? 1 : 0
+  source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
+  version = "~> 4"
+  name        =  var.custom_policy_name
+  path        = "/"
+  description = var.iam_policy_description
+  policy = var.iam_policy_document
+}
