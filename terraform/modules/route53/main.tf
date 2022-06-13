8a1eddf1c582e0b268ef0c759c4420a89899738f
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-06-13 11:36:24 -0400

    fixed hostd zone issue

--- terraform/modules/route53/main.tf
+++ terraform/modules/route53/main.tf
@@ -5,10 +5,10 @@ data "aws_route53_zone" "zone" {
 resource "aws_route53_record" "dns_record" {
   name = "${var.application_subdomain}-${var.env}"
   type = "A"
-  zone_id = var.alb_zone_id
+  zone_id = data.aws_route53_zone.zone.id
   alias {
     evaluate_target_health = false
     name = var.alb_dns_name
-    zone_id = data.aws_route53_zone.zone.id
+    zone_id = var.alb_zone_id
   }
 }
