184bffd4b89dde9682e20a463a153b76bb6abfe9
colemandevries <colemandevries@gmail.com>
2022-06-14 14:48:08 -0400

    Develop (#9)
    Added configurations for the existing OpenSearch module: cluster_config, domain_endpoint_options, encrypted_at_rest, and node_to_node_encryption
    the security-related configurations are not set up with variables. these need to have pre-defined defaults for security reasons.
    added variables to support the new configuration blocks
    reformatted and pushed

--- terraform/modules/opensearch/variables.tf
+++ terraform/modules/opensearch/variables.tf
@@ -1,48 +1,58 @@
 variable "tags" {
   description = "tags to associate with this instance"
-  type = map(string)
+  type        = map(string)
 }
 variable "stack_name" {
   description = "name of the project"
-  type = string
+  type        = string
 }
 
 variable "opensearch_instance_type" {
   description = "type of instance to be used to create the elasticsearch cluster"
-  type = string
-  default = "t3.medium.elasticsearch"
+  type        = string
+  default     = "t3.medium.elasticsearch"
 }
 
 variable "opensearch_version" {
-  type = string
+  type        = string
   description = "specify es version"
-  default = "OpenSearch_1.1"
+  default     = "OpenSearch_1.2"
 }
+
 variable "create_os_service_role" {
-  type = bool
-  default = false
+  type        = bool
+  default     = false
   description = "change this value to true if running this script for the first time"
 }
 variable "opensearch_subnet_ids" {
   description = "list of subnet ids to use"
-  type = list(string)
+  type        = list(string)
 }
 
 variable "env" {
   description = "name of the environment to provision"
-  type = string
+  type        = string
 }
 variable "automated_snapshot_start_hour" {
   description = "hour when automated snapshot to be taken"
-  type = number
-  default = 23
+  type        = number
+  default     = 23
 }
 variable "opensearch_security_group_ids" {
   description = "security group ids to apply to this resource"
-  type = list(string)
+  type        = list(string)
 }
 variable "opensearch_ebs_volume_size" {
   description = "size of the ebs volume attached to the opensearch instance"
-  type = number
-  default = 200
+  type        = number
+  default     = 200
+}
+
+variable "opensearch_instance_count" {
+  description = "the number of data nodes to provision for each instance in the cluster"
+}
+
+variable "multi_az_enabled" {
+  description = "set to true to enable multi-az deployment"
+  type        = bool
 }
