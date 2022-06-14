2a7088f9d556d00922be1a17129274e2c13c4e0c
colemandevries <colemandevries@gmail.com>
2022-06-14 16:39:40 -0400

    updated descriptions of opensearch outputs, testing terraform docs

--- terraform/modules/opensearch/outputs.tf
+++ terraform/modules/opensearch/outputs.tf
@@ -1,11 +1,14 @@
 output "opensearch_endpoint" {
   value = aws_opensearch_domain.es.endpoint
+  description = "the opensearch domain endpoint url"
 }
 
 output "opensearch_arn" {
   value = aws_opensearch_domain.es.arn
+  description = "the OpenSearch domain arn"
 }
 
 output "opensearch_cloudwatch_log_group_arn" {
   value = aws_cloudwatch_log_group.es.arn
+  description = "the log group arn that collects OpenSearch logs"
 }
