ce15651fdc033c898c46d6e9a5883a1a55b6b416
Cole DeVries <colemandevries@gmail.com>
2022-06-15 06:34:59 -0400

    modified S3 bucket module

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -1,14 +1,89 @@
-module "s3" {
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "3.2.3"
-  bucket = var.bucket_name
-  acl = var.bucket_acl
-  versioning = {
-    enabled = var.enable_version
+# module "s3" {
+#   source  = "terraform-aws-modules/s3-bucket/aws"
+#   version = "3.2.3"
+#   bucket = var.bucket_name
+#   acl = var.bucket_acl
+#   versioning = {
+#     enabled = var.enable_version
+#   }
+#   policy = var.bucket_policy
+#   lifecycle_rule = var.lifecycle_rule
+#   tags = var.tags
+#   attach_policy = var.attach_bucket_policy
+#   force_destroy = var.force_destroy_bucket
+# }
+
+resource "aws_s3_bucket" "s3" {
+  bucket        = local.bucket_name
+  force_destroy = var.s3_force_destroy
+  tags          = var.tags
+}
+
+resource "aws_s3_bucket_acl" "s3" {
+  bucket = aws_s3_bucket.s3.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "s3" {
+  bucket                  = aws_s3_bucket.s3.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+###### Need to decide if policy is passed in as variable or diff strategy?
+# resource "aws_s3_bucket_policy" "s3" {
+#   bucket = aws_s3_bucket.s3.id
+#   policy = data.aws_iam_policy_document.s3.json
+# }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3" {
+  bucket = aws_s3_bucket.s3.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
   }
-  policy = var.bucket_policy
-  lifecycle_rule = var.lifecycle_rule
-  tags = var.tags
-  attach_policy = var.attach_bucket_policy
-  force_destroy = var.force_destroy_bucket
+}
+
+resource "aws_s3_bucket_versioning" "s3" {
+  bucket = aws_s3_bucket.s3.id
+  
+  versioning_configuration {
+    status = "Enabled"
+  }
+
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "s3" {
+  bucket = aws_s3_bucket.s3.id
+
+  rule {
+    id     = "transition_to_standard_ia"
+    status = "Enabled"
+
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+
+    noncurrent_version_transition {
+      noncurrent_days = 30
+      storage_class   = "STANDARD_IA"
+    }
+  }
+
+  rule {
+    id     = "expire_objects"
+    status = "Enabled"
+
+    expiration {
+      days = 90
+    }
+
+    noncurrent_version_expiration {
+      noncurrent_days = 90
+    }
+  }
 }
--- terraform/modules/s3/outputs.tf
+++ terraform/modules/s3/outputs.tf
@@ -1,4 +1,4 @@
 output "bucket_name" {
-  value = module.s3.s3_bucket_id
+  value       = module.s3.s3_bucket_id
   description = "name of the bucket"
 }
