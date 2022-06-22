bf6d9b740fd7fa182b223d94f46ad794f163d60c
colemandevries <colemandevries@gmail.com>
2022-06-22 09:46:11 -0400

    updated autotune schedule to be less frequent, only occur on weekends

--- terraform/modules/opensearch/locals.tf
+++ terraform/modules/opensearch/locals.tf
@@ -1,6 +1,8 @@
 locals {
-  domain_name = "${var.stack_name}-${var.env}-opensearch"
-  subnets     = var.multi_az_enabled == true ? var.opensearch_subnet_ids : tolist(var.opensearch_subnet_ids[0])
+  domain_name    = "${var.stack_name}-${var.env}-opensearch"
+  subnets        = var.multi_az_enabled == true ? var.opensearch_subnet_ids : tolist(var.opensearch_subnet_ids[0])
   autotune_start = "2022-06-20T23:00:30.52Z"
-  autotune_reoccurance_cron = "0 59 23 ? 0 0"
+  #auto-tune cron format: minute hour day-of-month month day-of-week year
+  #autotune expression translation: 1:00 AM on saturdays between 2022 and 2024
+  autotune_reoccurance_cron = "0 1 ? * 6 2022-2024"
 }
