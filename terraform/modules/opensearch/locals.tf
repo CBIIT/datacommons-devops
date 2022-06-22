7f2cf212e1f02c683c21aede1c9e4de9835d67e6
colemandevries <colemandevries@gmail.com>
2022-06-22 10:12:49 -0400

    updated maintenance schedule start time to use terraform timestamp function

--- terraform/modules/opensearch/locals.tf
+++ terraform/modules/opensearch/locals.tf
@@ -1,7 +1,7 @@
 locals {
   domain_name    = "${var.stack_name}-${var.env}-opensearch"
   subnets        = var.multi_az_enabled == true ? var.opensearch_subnet_ids : tolist(var.opensearch_subnet_ids[0])
-  autotune_start = "2022-06-20T23:00:30.52Z"
+  autotune_start = timeadd(timestamp, "24h")
   #auto-tune cron format: minute hour day-of-month month day-of-week year
   #autotune expression translation: 1:00 AM on saturdays between 2022 and 2024
   autotune_reoccurance_cron = "0 1 ? * 6 2022-2024"
