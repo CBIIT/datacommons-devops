85b47f085f364dc3c63cbc9b4464af27bbb4f6ae
kiran <kotepalliv2@nih.gov>
2022-06-23 08:05:40 -0400

    updated ignore lifecyle for opensearch

--- terraform/modules/opensearch/main.tf
+++ terraform/modules/opensearch/main.tf
@@ -66,7 +66,9 @@ resource "aws_opensearch_domain" "os" {
 
   lifecycle {
     ignore_changes = [
-      auto_tune_options
+      "auto_tune_options.0.maintenance_schedule.start_at",
+      "auto_tune_options.0.maintenance_schedule.%",
+
     ]
   }
 }
