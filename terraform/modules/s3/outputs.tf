7506cf9ee0ec066965de83fecdc44f71fe9b02f3
kiran <kotepalliv2@nih.gov>
2022-06-23 13:29:10 -0400

    updated output

--- terraform/modules/s3/outputs.tf
+++ terraform/modules/s3/outputs.tf
@@ -1,4 +1,4 @@
 output "bucket_name" {
-  value       = aws_s3_bucket.s3.s3_bucket_id  #module.s3.s3_bucket_id
+  value       = aws_s3_bucket.s3.id  #module.s3.s3_bucket_id
   description = "name of the bucket"
 }
