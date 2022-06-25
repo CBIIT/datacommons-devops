9538718ee3bda1d1c92beeb397ae462ff68e3645
Cole DeVries <colemandevries@gmail.com>
2022-06-25 10:29:58 -0400

    reformatting only

--- terraform/modules/opensearch/locals.tf
+++ terraform/modules/opensearch/locals.tf
@@ -1,8 +1,8 @@
 locals {
-  domain_name    = "${var.stack_name}-${var.env}-opensearch"
-  subnets        = var.multi_az_enabled == true ? var.opensearch_subnet_ids : tolist(var.opensearch_subnet_ids[0])
-  now            = timestamp()
-  autotune_start = timeadd(local.now, "24h")
+  domain_name               = "${var.stack_name}-${var.env}-opensearch"
+  subnets                   = var.multi_az_enabled == true ? var.opensearch_subnet_ids : tolist(var.opensearch_subnet_ids[0])
+  now                       = timestamp()
+  autotune_start            = timeadd(local.now, "24h")
   autotune_reoccurance_cron = "cron(15 1 ? * 7 2021-2024)"
   sg_description            = "The security group regulating network access to the OpenSearch cluster"
   log_retention             = var.env == "dev" || var.env == "qa" ? 30 : 90
