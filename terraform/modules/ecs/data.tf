efbebe7c73760f61f4ff0e206ebc2c3f0e6d0e64
Cole DeVries <colemandevries@gmail.com>
2022-06-25 11:56:37 -0400

    Added ExecuteCommand, SSM, CloudWatch, S3, KMS policy docs for ecs_exec

--- terraform/modules/ecs/data.tf
+++ terraform/modules/ecs/data.tf
@@ -6,7 +6,7 @@ data "aws_region" "current" {
 
 }
 
-data "aws_iam_policy_document" "ecs_exec" {
+data "aws_iam_policy_document" "ecs_exec_command" {
   statement {
     effect    = "Allow"
     actions   = ["ecs:ExecuteCommand"]
@@ -17,5 +17,67 @@ data "aws_iam_policy_document" "ecs_exec" {
       values   = [var.stack_name]
       variable = "aws:ResourceTag/tag-value"
     }
+  }
+}
+
+data "aws_iam_policy_document" "ecs_exec_ssm" {
+
+  #refine for all SSM in account
+  statement {
+    effect = "Allow"
+    actions = [ 
+      "ssmmessages:CreateControlChannel",
+      "ssmmessages:CreateDataChannel",
+      "ssmmessages:OpenControlChannel",
+      "ssmmessages:OpenDataChannel"
+     ]
+     resources = [ "*" ]
+  }
+}
+
+data "aws_iam_policy_document" "ecs_exec_cloudwatch" {
+  
+  #need to refine this to all log groups in the account
+  statement {
+    effect = "Allow"
+    actions = [ 
+      "logs:DescribeLogGroups"
+     ]
+    resources = [ "*" ]
+  }
+
+  #need to refine this to exec log groups be referencing ARN in resources
+  statement {
+    effect = "Allow"
+    actions = [ 
+      "logs:CreateLogStream",
+      "logs:DescribeLogStreams",
+      "logs:PutLogEvents"
+     ]
+    resources = [ "*" ]
+  }
+}
+
+data "aws_iam_policy_document" "ecs_exec_s3" {
+
+  #need to conditionally create an S3 bucket for the logs? creating s3 should be something done outside of module?
+  statement {
+    effect = "Allow"
+    actions = [ "s3:PutObject" ]
+    resources = [ "${var.ecs_exec_log_bucket}/*" ]
+  }
+
+  #need to conditionally create an S3 bucket for the logs? creating s3 should be something done outside of module?
+  statement {
+    effect = "Allow"
+    actions = [ "s3:GetEncryptionConfiguration" ]
+    resources = var.ecs_exec_log_bucket
+  }
+
+  #refine for KMS key
+  statement {
+    effect = "Allow"
+    actions = [ "kms:Decrypt" ]
+    resources = [ "*" ]
   }
 }
