4526593a1a1e837c3292a8d9f1b09ee8daa89783
Cole DeVries <colemandevries@gmail.com>
2022-06-26 11:39:07 -0400

    updated the ecs kms iam policy

--- terraform/modules/ecs/data.tf
+++ terraform/modules/ecs/data.tf
@@ -31,7 +31,7 @@ data "aws_iam_policy_document" "task_execution_kms" {
       "kms:Decrypt",
       "kms:GenerateDataKey"
     ]
-    resources = ["*"]
+    resources = ["arn:aws:kms:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:key/*"]
   }
 }
 
