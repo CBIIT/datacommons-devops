a704bfa275a627ebc95b8e238c5073d767adfa5c
kiran <kotepalliv2@nih.gov>
2022-06-28 12:09:02 -0400

    updated terraform/modules/ecs/data.tf

--- terraform/modules/ecs/data.tf
+++ terraform/modules/ecs/data.tf
@@ -40,6 +40,7 @@ data "aws_iam_policy_document" "task_execution_kms" {
 }
 
 data "aws_iam_policy_document" "task_execution_secrets" {
+  statement {
   effect = "Allow"
   actions = [
     "secretsmanager:GetSecretValue",
@@ -49,6 +50,7 @@ data "aws_iam_policy_document" "task_execution_secrets" {
     "secretsmanager:GetResourcePolicy"
   ]
   resources = ["arn:aws:secretsmanager:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:secret:*"]
+  }
 }
 
 data "aws_iam_policy_document" "task_execution_ecr" {
