48f40a8ccee49933986b89cd769afad5833dc72f
kiran <kotepalliv2@nih.gov>
2022-06-28 12:18:30 -0400

    updated issues while using in project level

--- terraform/modules/ecs/securitygroups.tf
+++ terraform/modules/ecs/securitygroups.tf
@@ -18,7 +18,7 @@ resource "aws_security_group_rule" "nih_network_ingress" {
   protocol          = "tcp"
   from_port         = 80
   to_port           = 80
-  cidr_blocks       = [local.nih_cidr_ranges]
+  cidr_blocks       = local.nih_cidr_ranges
 
 }
 
