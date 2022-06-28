76992d719c339ecd176d3735a21d1a99a9676c9e
kiran <kotepalliv2@nih.gov>
2022-06-28 16:26:50 -0400

    updated outputs

--- terraform/modules/ecs/outputs.tf
+++ terraform/modules/ecs/outputs.tf
@@ -1,18 +1,17 @@
 # ECS Outputs:
 output "ecs_task_definition_arn" {
-  value = aws_ecs_task_definition.task.arn
+  value = {for k, v in aws_ecs_task_definition.task: k => v.arn}
 }
 
 output "ecs_task_service_arn" {
-  value = aws_ecs_service.service.arn
+  value = {for k, v in aws_ecs_service.service: k => v.arn}
 }
-
 output "appautoscaling_target_arn" {
-  value = aws_appautoscaling_target.microservice_autoscaling_target.arn
+  value = {for k, v in aws_appautoscaling_target.microservice_autoscaling_target: k => v.arn}
 }
 
 output "appautoscaling_policy_arn" {
-  value = aws_appautoscaling_policy.microservice_autoscaling_cpu.arn
+  value = {for k, v in aws_appautoscaling_policy.microservice_autoscaling_cpu: k => v.arn}
 }
 
 output "ecs_cluster_arn" {
