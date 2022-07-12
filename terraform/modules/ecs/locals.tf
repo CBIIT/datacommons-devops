062a6e617ef29af2ee8a8e8963045c4fa6a3bc8c
kiran <kotepalliv2@nih.gov>
2022-07-12 01:04:01 -0400

    updated terraform/modules/ecs/locals.tf

--- terraform/modules/ecs/locals.tf
+++ terraform/modules/ecs/locals.tf
@@ -5,7 +5,7 @@ locals {
   task_role_name                  = var.target_account_cloudone ? "${var.iam_prefix}-${var.stack_name}-${var.env}-ecs-task-role" : "${var.stack_name}-${var.env}-task-role"
   task_role_policy_exec_name      = var.target_account_cloudone ? "${var.iam_prefix}-${var.stack_name}-${var.env}-ecs-task-role-exec-policy" : "${var.stack_name}-${var.env}-task-role-exec-policy"
   task_execution_role_policy_name = var.target_account_cloudone ? "${var.iam_prefix}-${var.stack_name}-${var.env}-ecs-task-execution-role-policy" : "${var.stack_name}-${var.env}-task-execution-role-policy"
-  nih_cidr_ranges                 = var.target_account_cloudone ? ["129.43.0.0/16", "137.187.0.0/16", "10.128.0.0/9", "165.112.0.0/16", "156.40.0.0/16", "10.208.0.0/21", "128.231.0.0/16", "130.14.0.0/16", "157.98.0.0/16", "10.133.0.0/16"] : "${var.stack_name}-${var.env}-alb-sg"
+  nih_cidr_ranges                 = var.target_account_cloudone ? ["129.43.0.0/16", "137.187.0.0/16", "10.128.0.0/9", "165.112.0.0/16", "156.40.0.0/16", "10.208.0.0/21", "128.231.0.0/16", "130.14.0.0/16", "157.98.0.0/16", "10.133.0.0/16"] : []
   vpc_cidr                        = data.aws_vpc.current.cidr_block
   permission_boundary_arn         = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/PermissionBoundary_PowerUser"
 }
