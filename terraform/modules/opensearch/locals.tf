70a46a4d98db0a473b849aba50e2bc94d7324ebd
vdonkor <vincent.donkor@gmail.com>
2022-07-16 23:34:25 -0400

    fixing minor issues (#44)

--- terraform/modules/opensearch/locals.tf
+++ terraform/modules/opensearch/locals.tf
@@ -1,6 +1,6 @@
 locals {
   domain_name    = "${var.stack_name}-${var.env}-opensearch"
-  subnets        = var.multi_az_enabled == true ? var.opensearch_subnet_ids : tolist(var.opensearch_subnet_ids[0])
+  subnets        = var.multi_az_enabled ? var.opensearch_subnet_ids : [ var.opensearch_subnet_ids[0]]
   sg_description = "The security group regulating network access to the OpenSearch cluster"
   log_retention  = var.env == "dev" || var.env == "qa" ? 30 : 90
   now            = timestamp()
