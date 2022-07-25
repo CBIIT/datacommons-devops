5c5a2eba31478d9107c3026a97ca267e52496668
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-07-25 09:57:57 -0400

    adding ecr:GetAuthorizationToken

--- terraform/modules/ecs/data.tf
+++ terraform/modules/ecs/data.tf
@@ -69,7 +69,8 @@ data "aws_iam_policy_document" "task_execution_ecr" {
       "ecr:GetRepositoryPolicy",
       "ecr:InitiateLayerUpload",
       "ecr:ListTagsForResource",
-      "ecr:UploadLayerPart"
+      "ecr:UploadLayerPart",
+      "ecr:GetAuthorizationToken"
     ]
     resources = ["arn:aws:ecr:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:repository/*"]
   }
