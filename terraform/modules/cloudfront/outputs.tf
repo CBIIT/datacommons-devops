c35aaef881e98da3dc4213c5801559b038f9d8b3
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-09-27 22:34:04 -0400

    fixed kenesis

--- terraform/modules/cloudfront/outputs.tf
+++ terraform/modules/cloudfront/outputs.tf
@@ -1,3 +1,6 @@
 output "cloudfront_distribution_endpoint" {
   value = aws_cloudfront_distribution.distribution.domain_name
 }
+output "public_key" {
+  value = data.aws_secretsmanager_secret_version.cloudfront.secret_string["public-key"]
+}
