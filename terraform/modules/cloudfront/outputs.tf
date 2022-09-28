33da3b37a3637a26ca3f2c51e603b9de30aea49c
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-09-28 08:00:17 -0400

    refactored public

--- terraform/modules/cloudfront/outputs.tf
+++ terraform/modules/cloudfront/outputs.tf
@@ -1,6 +1,3 @@
 output "cloudfront_distribution_endpoint" {
   value = aws_cloudfront_distribution.distribution.domain_name
 }
-output "public_key" {
-  value = jsondecode(data.aws_secretsmanager_secret_version.cloudfront.secret_string)["publickey"]
-}
