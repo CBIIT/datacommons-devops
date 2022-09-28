e81ed88da4dfe52bec13c903248cdfb0822be209
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-09-27 22:03:13 -0400

    added fix for permission

--- terraform/modules/cloudfront/kinesis.tf
+++ terraform/modules/cloudfront/kinesis.tf
@@ -7,12 +7,17 @@ resource "aws_iam_role" "firehose_role" {
   name = local.kenesis_role_name
   assume_role_policy = data.aws_iam_policy_document.kinesis_assume_role_policy.json
   permissions_boundary = var.target_account_cloudone ? local.permission_boundary_arn: null
+  tags = var.tags
 }
-
-resource "aws_iam_role_policy" "firehose_policy" {
+resource "aws_iam_policy" "firehose_policy" {
   name = local.kenesis_policy_name
   policy = data.aws_iam_policy_document.firehose_policy.json
-  role = aws_iam_role.firehose_role.name
+  tags = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "firehose_policy_attachment" {
+  policy_arn =  aws_iam_policy.firehose_policy.arn
+  role       = aws_iam_role.firehose_role.name
 }
 
 resource "aws_kinesis_firehose_delivery_stream" "firehose_stream" {
