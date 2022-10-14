150ddf27a80b4d1c982920cf2fe802a77241440c
colemandevries <colemandevries@gmail.com>
2022-10-14 14:43:27 -0400

    adding the iam-read-only module

--- terraform/modules/firehose-metrics/modules/iam-read-only/main.tf
+++ terraform/modules/firehose-metrics/modules/iam-read-only/main.tf
@@ -6,13 +6,7 @@ resource "aws_iam_role" "read_only" {
   permissions_boundary  = var.permission_boundary_arn
 }
 
-resource "aws_iam_policy" "read_only" {
-  name        = "${var.iam_prefix}-${var.program}-${var.app}-${var.level}-metrics-read-only"
-  description = "Allows kenisis delivery streams to delivery failed messages to S3"
-  policy      = data.aws_iam_policy_document.read_only.json
-}
-
 resource "aws_iam_role_policy_attachment" "read_only" {
   role       = aws_iam_role.read_only.name
-  policy_arn = aws_iam_policy.read_only.arn
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
 }
---
+++ terraform/modules/firehose-metrics/modules/iam-read-only/variables.tf
@@ -0,0 +1,48 @@
+variable "account_id" {
+  type        = string
+  description = "Account ID for the deployment target - use 'data.aws_caller_identity.current.account_id"
+}
+
+variable "app" {
+  type        = string
+  description = "The name of the application (i.e. 'mtp')"
+}
+
+variable "program" {
+  type        = string
+  description = "The name of the program (i.e. 'ccdi')"
+}
+
+variable "iam_prefix" {
+  type        = string
+  description = "The string prefix for IAM resource name attributes"
+  default     = "power-user"
+}
+
+variable "level" {
+  type        = string
+  description = "The account level - either 'nonprod' or 'prod' are accepted"
+}
+
+variable "force_detach_policies" {
+  type        = bool
+  description = "Force detaching any policies the role has before destroying it"
+  default     = false
+}
+
+variable "new_relic_account_id" {
+  type        = string
+  description = "The account provided by New Relic during the account link registration process"
+  sensitive   = true
+}
+
+variable "new_relic_external_id" {
+  type        = string
+  description = "The external ID provided by New Relic during the account link registration process"
+  sensitive   = true
+}
+
+variable "permission_boundary_arn" {
+  type        = string
+  description = "The arn of the permission boundary associated with the role"
+}
