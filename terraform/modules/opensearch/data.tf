77d1bf9fc9ba86bc905df433a563cd45bd3efc2d
Michael Fleming <michael.fleming@nih.gov>
2022-11-04 15:17:47 -0400

    fixed resource name

--- terraform/modules/opensearch/data.tf
+++ terraform/modules/opensearch/data.tf
@@ -15,8 +15,8 @@ data "aws_iam_policy_document" "os_policy" {
       identifiers = ["es.amazonaws.com"]
     }
     resources = [
-      aws_cloudwatch_log_group.os.arn,
-      "${aws_cloudwatch_log_group.os.arn}:*"
+      aws_cloudwatch_log_group.os_logs.arn,
+      "${aws_cloudwatch_log_group.os_logs.arn}:*"
     ]
   }
 }
