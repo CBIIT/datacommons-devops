ca3ff4638d0480ec349dfca6cc0fecc3925d64c2
Michael Fleming <michael.fleming@nih.gov>
2022-11-07 10:26:46 -0500

    updated secrets module to store correct values

--- terraform/modules/secrets/secrets.tf
+++ terraform/modules/secrets/secrets.tf
@@ -7,15 +7,22 @@ resource "aws_secretsmanager_secret_version" "secrets_values" {
   secret_id     = aws_secretsmanager_secret.secrets.id
   secret_string = <<EOF
     {
-	  "es_host": "${var.es_endpoint}",
 	  "neo4j_user": "${var.neo4j_user}",
 	  "neo4j_password": "${var.neo4j_password}",
+	  "neo4j_ip": "${var.neo4j_ip}",
+	  "mysql_user": "${var.mysql_user}",
+	  "mysql_password": "${var.mysql_password}",
+	  "mysql_database": "${var.mysql_database}",
+	  "mysql_host": "${var.mysql_host}",
+	  "es_host": "${var.es_host}",
+	  "cookie_secret": "${var.cookie_secret}",
 	  "indexd_url": "${var.indexd_url}",
-	  "sumo_collector_endpoint": "collectors.fed.sumologic.com",
-	  "sumo_collector_token_be": "${var.sumo_collector_token_be}",
-	  "sumo_collector_token_fe": "${var.sumo_collector_token_fe}",
+	  "sumo_collector_endpoint": "${var.sumo_collector_endpoint}",
+	  "sumo_collector_token_frontend": "${var.sumo_collector_token_frontend}",
+	  "sumo_collector_token_backend": "${var.sumo_collector_token_backend}",
 	  "sumo_collector_token_files": "${var.sumo_collector_token_files}",
-	  "db_instance": "${var.db_instance}"
+	  "sumo_collector_token_users": "${var.sumo_collector_token_users}",
+	  "sumo_collector_token_auth": "${var.sumo_collector_token_auth}",
 	}
 EOF
 }
--- terraform/modules/secrets/variables.tf
+++ terraform/modules/secrets/variables.tf
@@ -2,10 +2,12 @@ variable "app" {
   description = "name of the app"
   type        = string
 }
-variable "es_endpoint" {
-  type        = string
-  description = "opensearch enpoint for this tier"
+variable "region" {
+  description = "aws region to use for this resource"
+  type = string
+  default = "us-east-1"
 }
+
 variable "neo4j_user" {
   type        = string
   description = "neo4j user"
@@ -15,23 +17,71 @@ variable "neo4j_password" {
   type        = string
   description = "neo4j password"
 }
+variable "neo4j_ip" {
+  type        = string
+  description = "name or IP address of the db instance for this tier"
+}
+
+variable "mysql_user" {
+  type        = string
+  description = "mysql user"
+  default     = ""
+}
+variable "mysql_password" {
+  type        = string
+  description = "mysql password"
+  default     = ""
+}
+variable "mysql_database" {
+  type        = string
+  description = "mysql database"
+  default     = ""
+}
+variable "mysql_host" {
+  type        = string
+  description = "mysql host"
+  default     = ""
+}
+variable "cookie_secret" {
+  type        = string
+  description = "cookie secret"
+  default     = ""
+}
+
+variable "es_host" {
+  type        = string
+  description = "opensearch enpoint for this tier"
+}
 variable "indexd_url" {
   type        = string
   description = "indexd url"
 }
-variable "sumo_collector_token_be" {
+
+variable "sumo_collector_endpoint" {
   type        = string
-  description = "Sumo collector token for backend"
+  description = "endpoint for Sumo collectors"
+  default     = "collectors.fed.sumologic.com"
 }
-variable "sumo_collector_token_fe" {
+variable "sumo_collector_token_frontend" {
   type        = string
   description = "Sumo collector token for frontend"
+}
+variable "sumo_collector_token_backend" {
+  type        = string
+  description = "Sumo collector token for backend"
 }
 variable "sumo_collector_token_files" {
   type        = string
   description = "Sumo collector token for files"
+  default     = ""
 }
-variable "db_instance" {
+variable "sumo_collector_token_users" {
   type        = string
-  description = "name of the db instance for this tier"
+  description = "Sumo collector token for users"
+  default     = ""
+}
+variable "sumo_collector_token_auth" {
+  type        = string
+  description = "Sumo collector token for auth"
+  default     = ""
 }
