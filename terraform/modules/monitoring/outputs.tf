53ce69c18ed408a8693a832d33616265fc18b8d9
Michael Fleming <michael.fleming@nih.gov>
2022-11-16 09:50:24 -0500

    updated additional regex

--- terraform/modules/monitoring/outputs.tf
+++ terraform/modules/monitoring/outputs.tf
@@ -3,10 +3,10 @@ output "frontend_source_url" {
   description = "frontend source url"
 }
 output "backend_source_url" {
-  value       = regex("([^/]+)(?:[^/]+$)", sumologic_http_source.backend_source.url)[0]
+  value       = regex(".*/(.*)", sumologic_http_source.backend_source.url)[0]
   description = "backend source url"
 }
 output "files_source_url" {
-  value       = regex("([^/]+)(?:[^/]+$)", sumologic_http_source.files_source.url)[0]
+  value       = regex(".*/(.*)", sumologic_http_source.files_source.url)[0]
   description = "files source url"
 }
