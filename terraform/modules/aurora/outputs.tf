39d0def59a0ad9fe277393dd692f60700ffd2223
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2023-02-14 09:52:15 -0500

    Update outputs.tf

--- terraform/modules/aurora/outputs.tf
+++ terraform/modules/aurora/outputs.tf
@@ -1,8 +1,8 @@
 
 output "cluster_endpoint" {
- value = aws_rds_cluster.rds.endpoint
+  value = aws_rds_cluster.rds.endpoint
 }
 output "db_password" {
- value = random_password.master_password.result
- sensitive = false
+  value = random_password.master_password.result
+  sensitive = false
 }
