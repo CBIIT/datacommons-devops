6c76725c00e52c717d9a9027e2cdb310c3faf5f0
Vincent Donkor <vincent.donkor@gmail.com>
2023-03-16 13:14:31 -0400

    updated neo4j modules

--- terraform/modules/neo4j/outputs.tf
+++ terraform/modules/neo4j/outputs.tf
@@ -2,8 +2,8 @@ output "private_ip" {
   value = aws_instance.db.private_ip
 }
 output "db_security_group_id" {
-  value = aws_security_group.database_sg.id
+  value = aws_security_group.database_sg[0].id
 }
 output "db_security_group_arn" {
-  value = aws_security_group.database_sg.arn
+  value = aws_security_group.database_sg[0].arn
 }
