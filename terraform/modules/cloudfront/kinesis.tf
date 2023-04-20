c6820aa90065336ddd8adbe37479b89b635d860c
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2023-04-20 15:17:03 -0400

    Update kinesis.tf

--- terraform/modules/cloudfront/kinesis.tf
+++ terraform/modules/cloudfront/kinesis.tf
@@ -8,28 +8,28 @@ resource "aws_iam_role" "firehose_role" {
   count  = var.create_kinesis ? 1:0
   name = local.kenesis_role_name
   assume_role_policy = data.aws_iam_policy_document.kinesis_assume_role_policy.json
-  permissions_boundary = var.target_account_cloudone ? local.permission_boundary_arn: null
+  permissions_boundary = var.target_account_cloudone && terraform.workspace == "dev" || terraform.workspace == "qa" ? local.permission_boundary_arn: null
   tags = var.tags
 }
 resource "aws_iam_policy" "firehose_policy" {
   count  = var.create_kinesis ? 1:0
   name = local.kenesis_policy_name
-  policy = data.aws_iam_policy_document.firehose_policy.json
+  policy = data.aws_iam_policy_document.firehose_policy[0].json
   tags = var.tags
 }
 
 resource "aws_iam_role_policy_attachment" "firehose_policy_attachment" {
-   count  = var.create_kinesis ? 1:0
-  policy_arn =  aws_iam_policy.firehose_policy.arn
-  role       = aws_iam_role.firehose_role.name
+  count  = var.create_kinesis ? 1:0
+  policy_arn =  aws_iam_policy.firehose_policy[0].arn
+  role       = aws_iam_role.firehose_role[0].name
 }
 
 resource "aws_kinesis_firehose_delivery_stream" "firehose_stream" {
-   count  = var.create_kinesis ? 1:0
+  count  = var.create_kinesis ? 1:0
   name        = "aws-waf-logs-${var.stack_name}-${var.env}-kinesis-firehose-stream"
   destination = "s3"
   s3_configuration {
-    role_arn   = aws_iam_role.firehose_role.arn
-    bucket_arn = aws_s3_bucket.kinesis_log.arn
+    role_arn   = aws_iam_role.firehose_role[0].arn
+    bucket_arn = aws_s3_bucket.kinesis_log[0].arn
   }
 }
