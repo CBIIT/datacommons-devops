0e7e45b1732c5940d6a6f730aee6c3e71e4b8e49
Vincent Donkor <vincent.donkor@gmail.com>
2023-05-24 12:30:14 -0400

    updated cds agents

--- terraform/modules/cloudfront/main.tf
+++ terraform/modules/cloudfront/main.tf
@@ -192,10 +192,10 @@ resource "aws_s3_bucket_public_access_block" "files_public_access" {
   restrict_public_buckets = true
 }
 
-#resource "aws_s3_bucket_public_access_block" "files_public_access" {
-#  bucket                  = aws_s3_bucket.access_logs.id
-#  block_public_acls       = true
-#  block_public_policy     = true
-#  ignore_public_acls      = true
-#  restrict_public_buckets = true
-#}
+resource "aws_s3_bucket_public_access_block" "files_public_access" {
+  bucket                  = aws_s3_bucket.access_logs.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
