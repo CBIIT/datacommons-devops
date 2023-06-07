467eed69989a577977d67d854f6e26e634cf66d1
Fleming, Michael (NIH/NCI) [C] <michael.fleming@nih.gov>
2023-06-07 10:08:17 -0400

    added policy docs for remote repos

--- terraform/modules/ecr/locals.tf
+++ terraform/modules/ecr/locals.tf
@@ -1,3 +1,9 @@
 locals {
   account_arn = format("arn:aws:iam::%s:root", data.aws_caller_identity.current.account_id)
+
+  # Access Policies
+  local       = var.access_scheme == "local" ? data.aws_iam_policy_document.local.json : ""
+  standard 	  = var.access_scheme == "standard" ? data.aws_iam_policy_document.standard.json : ""
+  alternate 	= var.access_scheme == "alternate" ? data.aws_iam_policy_document.alternate.json : ""
+  policy_doc  = coalesce(local.local, local.standard, local.alternate)
 }
