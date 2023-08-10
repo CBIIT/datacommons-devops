07e80ce2855e3141fd283be58c48e78f6857ae45
cloudprofessionals <mycloudprofessionals@gmail.com>
2023-08-10 18:06:10 -0400

    adding ecr account number

--- terraform/modules/ecs/variables.tf
+++ terraform/modules/ecs/variables.tf
@@ -125,7 +125,7 @@ variable "create_neo4j_db" {
 variable "use_local_account_id" {
   description = "use local account for ecr"
   type = bool
-  default = true
+  default = false
 }
 
 variable "central_ecr_account_id" {
