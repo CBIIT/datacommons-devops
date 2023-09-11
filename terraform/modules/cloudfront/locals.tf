20c942923516d1d1993559b5e4071530d7c5382a
cloudprofessionals <mycloudprofessionals@gmail.com>
2023-09-10 21:16:45 -0400

    testing changes

--- terraform/modules/cloudfront/locals.tf
+++ terraform/modules/cloudfront/locals.tf
@@ -1,6 +1,6 @@
 locals {
   s3_origin_id = "${var.stack_name}_files_origin_id"
-  env_type =  var.env == "dev" || var.env == "qa" ? "nonprod" : "prod"
+  env_type                = terraform.workspace
   kenesis_role_name       = var.target_account_cloudone ? "${var.iam_prefix}-${var.stack_name}-${var.env}-firehose-role" : "${var.stack_name}-${var.env}-firehose-role"
   kenesis_policy_name     = var.target_account_cloudone ? "${var.iam_prefix}-${var.stack_name}-${var.env}-firehose-policy" : "${var.stack_name}-${var.env}-firehose-policy"
   kenesis_bucket_name     = var.target_account_cloudone ? "cloudone-${var.stack_name}-${var.env}-kinesis-firehose-stream" : "${var.stack_name}-${var.env}-kinesis-firehose-stream"
