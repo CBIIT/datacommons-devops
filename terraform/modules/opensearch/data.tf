b18fc044f8a4a0d33f0c252409b9e47d3ebf0950
cloudprofessionals <mycloudprofessionals@gmail.com>
2023-10-13 13:47:33 -0400

    updated permission

--- terraform/modules/opensearch/data.tf
+++ terraform/modules/opensearch/data.tf
@@ -15,8 +15,7 @@ data "aws_iam_policy_document" "os" {
       identifiers = ["es.amazonaws.com"]
     }
     resources = [
-      aws_cloudwatch_log_group.os.arn,
-      "${aws_cloudwatch_log_group.os.arn}:*"
+      "${aws_cloudwatch_log_group.os.arn}:*:*:*"
     ]
   }
 }
