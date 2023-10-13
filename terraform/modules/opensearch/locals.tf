05becf3be4dc29a22e8135cbcf1bc7d67317adf5
cloudprofessionals <mycloudprofessionals@gmail.com>
2023-10-13 13:55:21 -0400

    updated opensearch

--- terraform/modules/opensearch/locals.tf
+++ terraform/modules/opensearch/locals.tf
@@ -1,7 +1,7 @@
 locals {
-  domain_name    = "${var.stack_name}-${var.env}-opensearch"
+  domain_name    = "${var.stack_name}-${terraform.workspace}-opensearch"
   subnets        = var.multi_az_enabled ? var.opensearch_subnet_ids : [ var.opensearch_subnet_ids[0]]
   sg_description = "The security group regulating network access to the OpenSearch cluster"
-  log_retention  = var.env == "dev" || var.env == "qa" ? 30 : 90
+  log_retention  = var.env == "dev2" || var.env == "dev" || var.env == "qa2" || var.env == "qa" ? 30 : 90
   now            = timestamp()
 }
