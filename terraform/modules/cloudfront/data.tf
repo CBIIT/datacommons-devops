a60931e4106b7230c069a824fb1287523b63453f
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2023-10-31 13:45:38 -0400

    Update data.tf

--- terraform/modules/cloudfront/data.tf
+++ terraform/modules/cloudfront/data.tf
@@ -94,7 +94,7 @@ data "aws_iam_policy_document" "lambda_s3_policy" {
       "s3:PutObject",
       "s3:GetObjectVersion",
     ]
-    resources = ["arn:aws:s3:::${aws_s3_bucket.kinesis_log.bucket}/*"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.kinesis_log[count.index].bucket}/*"]
   }
   statement {
     sid = ""
@@ -103,7 +103,7 @@ data "aws_iam_policy_document" "lambda_s3_policy" {
       "s3:ListBucket"
     ]
     resources = [
-      "arn:aws:s3:::${aws_s3_bucket.kinesis_log.bucket}"
+      "arn:aws:s3:::${aws_s3_bucket.kinesis_log[count.index].bucket}"
     ]
   }
   statement {
