6e04c66084d69301d36dcd3cef1ee3a876d8cd60
cloudprofessionals <mycloudprofessionals@gmail.com>
2023-11-09 15:34:55 -0500

    fixed typo

--- terraform/modules/ecs/iam.tf
+++ terraform/modules/ecs/iam.tf
@@ -1,7 +1,7 @@
 resource "aws_iam_role" "ecs_task_execution_role" {
   name                 = local.task_execution_role_name
   assume_role_policy   = var.use_custom_trust_policy ? var.custom_trust_policy: data.aws_iam_policy_document.ecs_trust_policy.json
-  permissions_boundary =  local.permission_boundary_arn : null
+  permissions_boundary = var.target_account_cloudone ? local.permission_boundary_arn : null
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_attachment" {
