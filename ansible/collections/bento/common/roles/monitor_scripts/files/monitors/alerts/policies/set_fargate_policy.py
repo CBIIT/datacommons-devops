32ad98f4db28b4543bf9ba073a48116bf8fa885f
Fleming, Michael (NIH/NCI) [C] <michael.fleming@nih.gov>
2023-11-16 13:59:59 -0500

    updated policy queries

--- ansible/collections/bento/common/roles/monitor_scripts/files/monitors/alerts/policies/set_fargate_policy.py
+++ ansible/collections/bento/common/roles/monitor_scripts/files/monitors/alerts/policies/set_fargate_policy.py
@@ -6,18 +6,40 @@ import requests
 from monitors.alerts.conditions.fargate import set_fargate_nrql_cpu_condition, set_fargate_nrql_mem_condition, set_fargate_nrql_restarts_condition
 
 def setpolicy(project, tier, key):
-   API_ENDPOINT = 'https://api.newrelic.com/v2/alerts_policies.json'
+   API_ENDPOINT = 'https://api.newrelic.com/graphql'
+   NR_ACCT_ID = os.getenv('NR_ACCT_ID')
 
    policy_name = '{} {} Fargate Policy'.format(project, tier)
    policy_found = False
-   headers = {'Api-Key': key}
+
+   headers = {
+     "Api-Key": key,
+     "Content-Type": "application/json"
+   }
    
+   data = """{{
+     actor {{
+       account(id: {}) {{
+         alerts {{
+           policiesSearch {{
+             policies {{
+               name
+               id
+             }}
+           }}
+         }}
+       }}
+     }}
+   }}""".format(NR_ACCT_ID)
+
+   payload = { "query": data }
+
    try:
-     response = requests.get('{}'.format(API_ENDPOINT), headers=headers)
+     response = requests.post('{}'.format(API_ENDPOINT), headers=headers, data=json.dumps(payload), allow_redirects=False)
    except requests.exceptions.RequestException as e:
      raise SystemExit(e)
 
-   for x in response.json()['policies']:
+   for x in response.json()['data']['actor']['account']['alerts']['policiesSearch']['policies']:
      if policy_name in x.get("name", "none"):
        policy_found = True
        policy_id = x.get("id", "none")
@@ -35,6 +57,20 @@ def setpolicy(project, tier, key):
    }
 
    if not policy_found:
+
+     API_ENDPOINT = 'https://api.newrelic.com/v2/alerts_policies.json'
+
+     headers = {
+       "Api-Key": key,
+       "Content-Type": "application/json"
+     }
+
+     data = {
+       "policy": {
+         "incident_preference": "PER_POLICY",
+         "name": policy_name
+       }
+     }
 
      # create policy
      try:
--- ansible/collections/bento/common/roles/monitor_scripts/files/monitors/alerts/policies/set_synthetics_policy.py
+++ ansible/collections/bento/common/roles/monitor_scripts/files/monitors/alerts/policies/set_synthetics_policy.py
@@ -6,35 +6,60 @@ import requests
 #from monitors.alerts.conditions.fargate import set_fargate_nrql_cpu_condition, set_fargate_nrql_mem_condition, set_fargate_nrql_restarts_condition
 
 def setpolicy(project, tier, key):
-   API_ENDPOINT = 'https://api.newrelic.com/v2/alerts_policies.json'
+   API_ENDPOINT = 'https://api.newrelic.com/graphql'
+   NR_ACCT_ID = os.getenv('NR_ACCT_ID')
 
    policy_name = '{} {} Synthetics Policy'.format(project, tier)
    policy_found = False
-   headers = {'Api-Key': key}
-   
-   try:
-     response = requests.get('{}'.format(API_ENDPOINT), headers=headers)
-   except requests.exceptions.RequestException as e:
-     raise SystemExit(e)
-
-   for x in response.json()['policies']:
-     if policy_name in x.get("name", "none"):
-       policy_found = True
-       policy_id = x.get("id", "none")
 
    headers = {
      "Api-Key": key,
      "Content-Type": "application/json"
    }
    
-   data = {
-     "policy": {
-       "incident_preference": "PER_POLICY",
-       "name": policy_name
-     }
-   }
+   data = """{{
+     actor {{
+       account(id: {}) {{
+         alerts {{
+           policiesSearch {{
+             policies {{
+               name
+               id
+             }}
+           }}
+         }}
+       }}
+     }}
+   }}""".format(NR_ACCT_ID)
+
+   payload = { "query": data }
+
+   try:
+     response = requests.post('{}'.format(API_ENDPOINT), headers=headers, data=json.dumps(payload), allow_redirects=False)
+   except requests.exceptions.RequestException as e:
+     raise SystemExit(e)
+
+   #for x in response.json()['policies']:
+   for x in response.json()['data']['actor']['account']['alerts']['policiesSearch']['policies']:
+     if policy_name in x.get("name", "none"):
+       policy_found = True
+       policy_id = x.get("id", "none")
 
    if not policy_found:
+
+     API_ENDPOINT = 'https://api.newrelic.com/v2/alerts_policies.json'
+
+     headers = {
+       "Api-Key": key,
+       "Content-Type": "application/json"
+     }
+
+     data = {
+       "policy": {
+         "incident_preference": "PER_POLICY",
+         "name": policy_name
+       }
+     }
 
      # create policy
      try:
