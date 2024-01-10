2f6a7b63d92f889a40d35de620663ce43929c52e
Kotepalli <kotepalliv2@nih.gov>
2024-01-10 13:32:35 -0500

    updating params for opensearch restore

--- ansible/collections/bento/common/roles/opensearch_dataloading/files/opensearch-restore.py
+++ ansible/collections/bento/common/roles/opensearch_dataloading/files/opensearch-restore.py
@@ -2,6 +2,7 @@ import argparse
 import boto3
 import requests
 from requests_aws4auth import AWS4Auth
+import time
 
 parser = argparse.ArgumentParser(description='Opensearch Backup Script')
 parser.add_argument("--oshost", type=str, help="opensearch host with trailing /")
@@ -34,41 +35,43 @@ payload = {
   "type": "s3",
   "settings": {
     "bucket": s3bucket,
-    "base_path": "os-backup",
+    "base_path": basepath,
     "region": "us-east-1",
     "role_arn": rolearn
   }
 }
 
 headers = {"Content-Type": "application/json"}
-
+print("registering repo")
 r = requests.put(url, auth=awsauth, json=payload, headers=headers)
 
 print(r.status_code)
 print(r.text)
+time.sleep(100) 
 
-payload = {
+headers = {"Content-Type": "application/json"}
+print("starting deleting the indices")
+indice_arr = indices.split(",")
+for i in indice_arr:
+  check = requests.get(oshost+i, auth=awsauth, headers=headers)
+  if check.status_code==200:
+    r = requests.delete(oshost+i, auth=awsauth, headers=headers)
+    print(r.text)
+
+print("finished deleting the indices, waiting 2 mins for the deletion to complete")
+time.sleep(120) 
+#
+print("started restore the indices")
+payload_restore = {
   "indices": indices,
   "include_global_state": False
 }
 path = '_snapshot/'+repo+'/'+snapshot+'/_restore'
-
-
-headers = {"Content-Type": "application/json"}
-print("starting deleting the indices")
-print(r.text)
-print("finished deleting the indices")
-#
-print("started restore the indices")
-payload = {
-  "type": "s3",
-  "settings": {
-    "bucket": s3bucket,
-    "region": "us-east-1",
-    "role_arn": rolearn
-  }
-}
+print(path)
 print(payload)
-r = requests.post(oshost+path, auth=awsauth, json=payload, headers=headers)
+
+r = requests.post(oshost+path, auth=awsauth, json=payload_restore, headers=headers)
 #
 print(r.text)
+if r.status_code!=200:
+  raise Exception("Sorry, pipeline does not run successfully")
