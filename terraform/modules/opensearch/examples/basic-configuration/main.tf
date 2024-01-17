638a83941bd36890ba5ef9da1a4a72e26bf3a542
Cole DeVries <colemandevries@gmail.com>
2024-01-17 13:52:01 -0500

    added basic usage

---
+++ terraform/modules/opensearch/examples/basic-configuration/main.tf
@@ -0,0 +1,11 @@
+module "opensearch" {
+  source = "../.."
+
+  domain_name                 = "program-tier-app-opensearch"
+  attach_permissions_boundary = true
+  cluster_tshirt_size         = "md"
+  engine_version              = "OpenSearch_2.11"
+  s3_snapshot_bucket_arn      = "arn:aws:s3:::basic-example-snapshot-bucket"
+  subnet_ids                  = ["subnet-01234567891011121", "subnet-abcdefghijklmnopq"]
+  vpc_id                      = "vpc-01234567891011121"
+}
