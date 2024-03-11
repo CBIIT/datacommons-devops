f7ee9af54d9a5c414765a2e7d09c306bc1cca034
Cole DeVries <colemandevries@gmail.com>
2024-03-11 14:28:14 -0400

    updated the address of the instance resource in outputs by removing iterators

--- terraform/modules/neptune-cluster/outputs.tf
+++ terraform/modules/neptune-cluster/outputs.tf
@@ -53,43 +53,43 @@ output "kms_key_arn" {
 }
 
 output "instance_address" {
-  value       = aws_neptune_cluster_instance.this[0].address
+  value       = aws_neptune_cluster_instance.this.address
   description = "The hostname of the instance. See also endpoint and port."
   sensitive   = false
 }
 
 output "instance_arn" {
-  value       = aws_neptune_cluster_instance.this[0].arn
+  value       = aws_neptune_cluster_instance.this.arn
   description = "The ARN of the neptune instance"
   sensitive   = false
 }
 
 output "instance_cluster_identifier" {
-  value       = aws_neptune_cluster_instance.this[0].cluster_identifier
+  value       = aws_neptune_cluster_instance.this.cluster_identifier
   description = "The neptune cluster identifier"
   sensitive   = false
 }
 
 output "instance_dbi_resource_id" {
-  value       = aws_neptune_cluster_instance.this[0].instance_dbi_resource_id
+  value       = aws_neptune_cluster_instance.this.instance_dbi_resource_id
   description = "The neptune instance resource ID"
   sensitive   = false
 }
 
 output "instance_endpoint" {
-  value       = aws_neptune_cluster_instance.this[0].endpoint
+  value       = aws_neptune_cluster_instance.this.endpoint
   description = "The hostname of the instance. See also address and port."
   sensitive   = false
 }
 
 output "instance_id" {
-  value       = aws_neptune_cluster_instance.this[0].id
+  value       = aws_neptune_cluster_instance.this.id
   description = "The neptune instance ID"
   sensitive   = false
 }
 
 output "instance_identifier" {
-  value       = aws_neptune_cluster_instance.this[0].identifier
+  value       = aws_neptune_cluster_instance.this.identifier
   description = "The neptune instance identifier"
   sensitive   = false
 }
