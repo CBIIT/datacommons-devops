c96a98b427991ff1330966971bf041107b20face
Cole DeVries <colemandevries@gmail.com>
2024-03-27 11:02:24 -0400

    updated the outputs to have count iterators

--- terraform/modules/neptune-cluster/outputs.tf
+++ terraform/modules/neptune-cluster/outputs.tf
@@ -47,7 +47,7 @@ output "cluster_reader_endpoint" {
 }
 
 output "kms_key_arn" {
-  value       = aws_kms_key.this[0].arn
+  value       = local.kms_key_arn
   description = "the neptune cluster kms key arn"
   sensitive   = false
 }
