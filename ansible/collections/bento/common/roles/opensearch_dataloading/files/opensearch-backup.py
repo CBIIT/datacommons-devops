f622cadbac54a8eb0df520ab7ed01449227ab44a
Fleming, Michael (NIH/NCI) [C] <michael.fleming@nih.gov>
2024-05-06 09:19:18 -0400

    split snapshot script into functions

--- ansible/collections/bento/common/roles/opensearch_dataloading/files/opensearch-backup.py
+++ ansible/collections/bento/common/roles/opensearch_dataloading/files/opensearch-backup.py
@@ -3,85 +3,100 @@ import boto3
 import requests
 from requests_aws4auth import AWS4Auth
 
-parser = argparse.ArgumentParser(description='Opensearch Backup Script')
-parser.add_argument("--oshost", type=str, help="opensearch host with trailing /")
-parser.add_argument("--repo", type=str, help="opensearch snapshot repository")
-parser.add_argument("--s3bucket", type=str, help="s3 bucket")
-parser.add_argument("--basePath", type=str, help="s3 bucket base path", nargs='?', const='')
-parser.add_argument("--snapshot", type=str, help="opensearch snapshot value")
-parser.add_argument("--region", type=str, help="region")
-parser.add_argument("--rolearn", type=str, help="role arn - typically power user role")
-args = parser.parse_args()
-print(args)
-oshost = args.oshost
-repo = args.repo
+def getArgs():
+  parser = argparse.ArgumentParser(description='Opensearch Backup Script')
+  parser.add_argument("--oshost", type=str, help="opensearch host with trailing /")
+  parser.add_argument("--repo", type=str, help="opensearch snapshot repository")
+  parser.add_argument("--s3bucket", type=str, help="s3 bucket")
+  parser.add_argument("--snapshot", type=str, help="opensearch snapshot value")
+  parser.add_argument("--indices", type=str, help="indices", nargs='?', const='')
+  parser.add_argument("--rolearn", type=str, help="role arn - typically power user role")
+  parser.add_argument("--basepath", type=str, help="basepath", nargs='?', const='')
+  parser.add_argument("--region", type=str, help="region")
+  args = parser.parse_args()
 
-s3bucket= args.s3bucket
-snapshot= args.snapshot 
-rolearn= args.rolearn
+  argList = {}
+  argList['oshost'] = args.oshost
+  argList['repo'] = args.repo
+  argList['s3bucket'] = args.s3bucket
+  argList['snapshot'] = args.snapshot 
+  argList['indices'] = args.indices
+  argList['rolearn'] = args.rolearn
+  argList['region'] = args.region
 
-basepath= args.basePath
-if basepath :
-  basepath = basepath + '/' + snapshot
-else:
-  basepath = snapshot
+  basepath = args.basepath
+  if basepath :
+    argList['basepath'] = basepath + '/' + argList['snapshot']
+  else:
+    argList['basepath'] = argList['snapshot']
 
-host = (oshost) 
-region = args.region
-service = 'es'
-credentials = boto3.Session().get_credentials()
-awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+  return argList
 
-# Register repository
-headers = {"Content-Type": "application/json"}
-path = '_snapshot/'+repo
-url = host + path
-payload = {
-  "type": "s3",
-  "settings": {
-    "bucket": s3bucket,
-    "base_path": basepath,
-    "region": region,
-    "role_arn": rolearn,
-    "canned_acl": "bucket-owner-full-control"
+
+def osAuth(argList):
+  # Opensearch authentication
+  service = 'es'
+  credentials = boto3.Session().get_credentials()
+  awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, argList['region'], service, session_token=credentials.token)
+
+  return awsauth
+
+def registerRepo(argList, awsauth):
+
+  # Registering Repo
+  path = '_snapshot/' + argList['repo']
+  url = argList['oshost'] + path
+
+  payload = {
+    "type": "s3",
+    "settings": {
+      "bucket": argList['s3bucket'],
+      "base_path": argList['basepath'],
+      "region": argList['region'],
+      "role_arn": argList['rolearn'],
+      "canned_acl": "bucket-owner-full-control"
+    }
   }
-}
 
-#create repo if not present
-print(payload)
-print("check repo")
-# r_get_repo = requests.get(url, auth=awsauth, json=payload, headers=headers)
-# if(r_get_repo.status_code!=200):
-#   print("repo does not exist, creating it")
-#   r_create_repo= requests.put(url, auth=awsauth, json=payload, headers=headers)
-#   print(r_create_repo.status_code)
-#   print(r_create_repo.text)
+  headers = {"Content-Type": "application/json"}
+  print("registering repo")
+  try:
+    r = requests.put(url, auth=awsauth, json=payload, headers=headers)
+    #time.sleep(100)
+  except requests.exceptions.RequestException as e:
+    raise SystemExit(e)
+  
 
-# print(r_get_repo.status_code)
-# print(r_get_repo.text)
+def createSnapshot(argList, awsauth):
+  # Create Snapshot
+  snapshot_url = argList['oshost'] + '_snapshot/' + argList['repo'] + '/' + argList['snapshot'] + '/'
+  print(snapshot_url)
 
-r_create_repo= requests.put(url, auth=awsauth, json=payload, headers=headers)
-
-#snapshot
-snapshot_path = '_snapshot/' + repo+'/' + snapshot+'/'
-print(snapshot_path) 
-url_create_snapshot = host + snapshot_path
-payload = {
-  "type": "s3",
-  "settings": {
-    "bucket": s3bucket,
-    "base_path": basepath,
-    "region": region,
-    "role_arn": rolearn,
-    "canned_acl": "bucket-owner-full-control"
+  headers = {"Content-Type": "application/json"}
+  payload = {
+    "type": "s3",
+    "settings": {
+      "indices": argList['indices'],
+      "bucket": argList['s3bucket'],
+      "base_path": argList['basepath'],
+      "region": argList['region'],
+      "role_arn": argList['rolearn'],
+      "canned_acl": "bucket-owner-full-control"
+    }
   }
-}
+
+  print("taking opensearch snapshot")
+  result = requests.put(snapshot_url, auth=awsauth, json=payload, headers=headers)
+
+  return result
 
 
-print("taking opensearch snapshot")
-r = requests.put(url_create_snapshot, auth=awsauth, json=payload, headers=headers)
+if __name__ == "__main__":
+   argList = getArgs()
+   awsauth = osAuth(argList)
+   registerRepo(argList, awsauth)
 
-print(r.status_code)
-print(r.text)
-if r.status_code!=200:
-  raise Exception("Sorry, pipeline does not run successfully")
+   result = createSnapshot(argList, awsauth)
+   print(result.text)
+   if result.status_code!=200:
+    raise Exception("Sorry, pipeline does not run successfully")
