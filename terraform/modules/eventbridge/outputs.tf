305086f311bc242844a3d8fd506d00a98c08abaf
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2024-05-13 14:17:29 -0400

    updated

--- terraform/modules/eventbridge/outputs.tf
+++ terraform/modules/eventbridge/outputs.tf
@@ -1,7 +1,19 @@
+# Output for the EventBridge rule name
 output "eventbridge_rule_name" {
   value = aws_cloudwatch_event_rule.module_event.name
+  description = "The name of the EventBridge rule."
 }
 
-output "eventbridge_target_id" {
-  value = aws_cloudwatch_event_target.module_target.target_id
+# Output for the ECS EventBridge target ID, conditionally output if ECS target is created
+output "eventbridge_ecs_target_id" {
+  value       = aws_cloudwatch_event_target.ecs_target[0].target_id
+  description = "The ID of the EventBridge target for the ECS task."
+  condition   = length(aws_cloudwatch_event_target.ecs_target) > 0
+}
+
+# Output for the Lambda EventBridge target ID, conditionally output if Lambda target is created
+output "eventbridge_lambda_target_id" {
+  value       = aws_cloudwatch_event_target.lambda_target[0].target_id
+  description = "The ID of the EventBridge target for the Lambda function."
+  condition   = length(aws_cloudwatch_event_target.lambda_target) > 0
 }
