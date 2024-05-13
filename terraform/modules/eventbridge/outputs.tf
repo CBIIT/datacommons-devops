bbc66bde7f75b2554cd4ce69c5458d2696fa4a97
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2024-05-13 13:31:25 -0400

    updated

--- terraform/modules/eventbridge/outputs.tf
+++ terraform/modules/eventbridge/outputs.tf
@@ -1,7 +1,7 @@
 output "eventbridge_rule_name" {
-  value = aws_eventbridge_rule.module_event.name
+  value = aws_cloudwatch_event_rule.module_event.name
 }
 
 output "eventbridge_target_id" {
-  value = aws_eventbridge_target.module_target.target_id
+  value = aws_cloudwatch_event_target.module_target.target_id
 }
