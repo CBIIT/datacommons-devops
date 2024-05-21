55dd1592455a5142f285fad3ef96d96b067d7210
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2024-05-21 16:56:28 -0400

    updated

--- terraform/modules/eventbridge/data.tf
+++ terraform/modules/eventbridge/data.tf
@@ -1,5 +1,5 @@
 data "aws_caller_identity" "current" {}
 
-data "aws_ecs_task_definition" "latest" {
+/*data "aws_ecs_task_definition" "latest" {
   task_definition  = var.task_definition_arn
-}
+}*/
