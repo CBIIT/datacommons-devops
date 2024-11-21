25b160b18bd71259577bfa97cade3b683c6e93c0
Kotepalli <kotepalliv2@nih.gov>
2024-11-21 12:16:30 -0500

    updated s3_configuration outside http_endpoint_configuration

--- terraform/modules/firehose-metrics/modules/kinesis-firehose-datastream/main.tf
+++ terraform/modules/firehose-metrics/modules/kinesis-firehose-datastream/main.tf
@@ -14,16 +14,15 @@ resource "aws_kinesis_firehose_delivery_stream" "kinesis" {
     request_configuration {
       content_encoding = var.content_encoding
     }
-
-    s3_configuration {
-      role_arn            = var.role_arn
-      bucket_arn          = var.s3_bucket_arn
-      prefix              = var.s3_object_prefix
-      error_output_prefix = var.s3_error_output_prefix
-      #buffer_size         = var.buffer_size
-      #buffer_interval     = var.buffer_interval
-      compression_format  = var.s3_compression_format
-    }
+  }
+  s3_configuration {
+    role_arn            = var.role_arn
+    bucket_arn          = var.s3_bucket_arn
+    prefix              = var.s3_object_prefix
+    error_output_prefix = var.s3_error_output_prefix
+    #buffer_size         = var.buffer_size
+    #buffer_interval     = var.buffer_interval
+    compression_format  = var.s3_compression_format
   }
 }
 
