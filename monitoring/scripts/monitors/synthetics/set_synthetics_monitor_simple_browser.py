943e430dee476c8a16f02caf8e5f6181fbc477d9
Fleming, Michael (NIH/NCI) [C] <michael.fleming@nih.gov>
2025-09-08 14:49:56 -0400

    updated location setting

--- monitoring/scripts/monitors/synthetics/set_synthetics_monitor_simple_browser.py
+++ monitoring/scripts/monitors/synthetics/set_synthetics_monitor_simple_browser.py
@@ -16,7 +16,8 @@ def setsyntheticsmonitor(project, tier, key, api, policy_id):
    else:
      freq = 'EVERY_30_MINUTES'
    
-   if api['location'].lower() in ['true'] and tier.lower() != 'prod':
+  #  if api['location'].lower() in ['true'] and tier.lower() != 'prod':
+   if api['location'].lower() in ['true']:
      location = "private: [\"" + os.getenv('LOCATION') + "\"]"
    else:
      location = "public: [\"AWS_US_EAST_1\"]"
