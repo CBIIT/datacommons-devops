40e15f525ee94b756c728732ed751e4aa4222f9b
Fleming, Michael (NIH/NCI) [C] <michael.fleming@nih.gov>
2025-09-17 17:17:41 -0400

    fixed scripted api private monitor

--- monitoring/scripts/monitors/synthetics/set_synthetics_monitor_scripted_api.py
+++ monitoring/scripts/monitors/synthetics/set_synthetics_monitor_scripted_api.py
@@ -16,7 +16,8 @@ def setsyntheticsmonitor(project, tier, key, api, policy_id):
    else:
      freq = 'EVERY_30_MINUTES'
 
-   if api['location'].lower() in ['true'] and tier.lower() != 'prod':
+  #  if api['location'].lower() in ['true'] and tier.lower() != 'prod':
+  if api['location'].lower() in ['true']:
      location = '''private: {{
        guid: "{guid}",
        vsePassword: "{password}"
