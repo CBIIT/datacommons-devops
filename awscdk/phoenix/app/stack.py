f79e3fcb944b980660cbaf43c4bd4042299ddf27
Fleming, Michael (NIH/NCI) [C] <michael.fleming@nih.gov>
2026-07-10 11:49:01 -0400

    updated for custom dockerfile additions

--- awscdk/phoenix/app/stack.py
+++ awscdk/phoenix/app/stack.py
@@ -4,7 +4,7 @@ from aws_cdk import Stack, Duration, RemovalPolicy
 from aws_cdk import aws_ec2 as ec2
 from aws_cdk import aws_ecs as ecs
 from aws_cdk import aws_kms as kms
-# from aws_cdk import aws_iam as iam
+from aws_cdk import aws_iam as iam
 from aws_cdk import aws_rds as rds
 from aws_cdk import aws_elasticloadbalancingv2 as elbv2
 from aws_cdk import aws_certificatemanager as cfm
@@ -112,16 +112,37 @@ class Stack(Stack):
         taskDefinition = ecs.FargateTaskDefinition(self,
             "{}-{}-taskDef".format(self.namingPrefix, service),
             cpu=config.getint(service, 'cpu'),
-            memory_limit_mib=config.getint(service, 'memory')
+            memory_limit_mib=config.getint(service, 'memory'),
+            runtime_platform=ecs.RuntimePlatform(
+                cpu_architecture=ecs.CpuArchitecture.ARM64,
+                operating_system_family=ecs.OperatingSystemFamily.LINUX,
+            ),
         )
+
+        bedrockPolicy = iam.ManagedPolicy(
+            self, "BedrockInvokePolicy",
+            statements=[
+                iam.PolicyStatement(
+                    effect=iam.Effect.ALLOW,
+                    actions=[
+                        "bedrock:InvokeModel",
+                        "bedrock:InvokeModelWithResponseStream",
+                    ],
+                    resources=[
+                        f"arn:aws:bedrock:{self.region}::foundation-model/anthropic.claude-*",
+                    ],
+                )
+            ],
+        )
+
+        taskDefinition.task_role.add_managed_policy(bedrockPolicy)
 
         phoenixContainer = taskDefinition.add_container(
             service,
-            image=ecs.ContainerImage.from_registry(config[service]['image']),
+            image=ecs.ContainerImage.from_asset("phoenix-image"),
             cpu=config.getint(service, 'cpu'),
             memory_limit_mib=config.getint(service, 'memory'),
             port_mappings=[ecs.PortMapping(container_port=config.getint(service, 'port'), name=service)],
-            # user="root",
             entry_point=entry_point,
             secrets=secrets,
             environment=environment,
